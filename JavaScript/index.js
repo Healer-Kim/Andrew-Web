9c0a59315d27deab20fb457b4b8d62a0bbb97bc8
Healer Kim <andrewc624@gmail.com>
2021-11-12 09:02:21 +0900

    started a new lesson with javascript, added a homework for week 10 11/12

---
+++ JavaScript/index.js
@@ -0,0 +1,99 @@
+// //My age
+// console.log(18); 
+// //Number of siblings I have
+// console.log(0); 
+// /* 
+// First excercse to using console.log and comments to print strings and intergers on javascript
+
+// Select a section of code & ctrl + / to instantly indent as commnent
+// */
+
+// console.log(18); 
+// console.log(0);
+
+// console.log("Javascript"); // Prints out a string Javascript
+// console.log(2011); // Prints out an integer 2011
+// console.log("Logging any sentence with a quotation"); // sentence string
+// console.log(1.2); // boolean
+
+
+
+// console.log(18 + 3.5)
+// console.log(2021-1969)
+// console.log(65/240)
+// console.log(0.2708 * 100)
+
+// console.log("Hello " + "Students".length )
+
+// console.log("abcdefg".length)
+
+// console.log("apple".toUpperCase())
+
+// console.log("APPLE".toLowerCase("he")) 
+
+// console.log("hello".startsWith("h"))
+
+// console.log("hello".startsWith("a"))
+
+// console.log("hello".startsWith("He")) //case sensitive
+
+// console.log(Math.random() * 10);
+
+// console.log(Math.ceil(1.6) * 2);
+
+// console.log(Math.ceil(1.6 * 2));
+
+// let x = true
+// console.log(x);
+
+// let levelUp = 10;
+// levelUp += 5;
+// levelUp -= 100;
+// levelUp *= 11;
+// levelUp %= 2;
+
+// var x = new Boolean(true)
+
+// loop:while(x = true);
+//     console.log("Looping");
+//     setTimeout(5000);
+//     var x = new Boolean(false)
+
+
+let gainedDollar = 3;
+let lostDollar = 50;
+
+gainedDollar++; // increment operator
+lostDollar--; // decrement operator
+
+console.log(gainedDollar, lostDollar); // increment/decrement results
+
+let favoriteAnimal= "Cat";
+const myMessage = 'My favorite animal: ' + favoriteAnimal ;
+console.log(myMessage);
+
+const myPet = 'armadillo'
+console.log(`I own a pet ${myPet}.`);
+// output: I own a pet armadillo.   
+
+
+/*
+I have printed blah blah blah blah blah blah blah blah 
+blah blah blah blah blah blah blah blah blah blah
+*/
+const myName = 'Andrew Chang';
+const myCity = 'Chicago';
+const myFavorites = `My name is ${myName}. My favorite city is ${myCity}.`;
+console.log(myFavorites);
+
+/*
+Checking type of operators
+*/
+
+const unknown1 = 'foo';
+
+const unknown2 = '10';
+
+const unknown3 = 'true';
+
+console.log(typeOf(unknown1));
